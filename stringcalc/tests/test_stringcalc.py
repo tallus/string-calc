814b43b495d9e389f977bc3d33263a091ac64aab
Paul Munday <contactme@paulmunday.net>
2017-07-22 15:07:48 -0700

    initial commit

---
+++ stringcalc/tests/test_stringcalc.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+copyright (c) 2017 Paul Munday.
+All rights reserved
+..codeauthor::Paul Munday <paul@paulmunday.net>
+
+[ INSERT DOC STRING ]  # TODO
+"""
+
+# Imports from Standard Library
+import unittest
+
+import mock
+
+# Local Imports
+
+# Constants
+
+# Helper Functions & Classes
+
+# Tests
+class MyTests(unittest.TestCase):
+    """[ INSERT DOCSTRING ]"""  # TODO
+    # pylint: disable=R0904
+
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+    def test_example(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == "__main__":
+    unittest.main()
